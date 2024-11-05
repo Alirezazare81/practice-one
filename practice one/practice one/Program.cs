60d93b68e31f4f2140e8afd696023697144f544b
Alirezazare81 <zare81180488@gmail.com>
2024-11-05 20:36:46 +0330

    tamrin 1
    alireza zare Az barname nevisi eram

---
+++ practice one/practice one/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int adad1, adad2, miangin;
+            Console.WriteLine("enter number one");
+            adad1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter number two");
+            adad2 = int.Parse(Console.ReadLine());
+            miangin = (adad1 + adad2) / 2;
+            Console.WriteLine("miangin:   "+miangin);
+            Console.ReadKey();
+
+        }
+    }
+}
